dd4fe830cdcba3a61b3087e59249fe76007e9042
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-02-12 10:28:40 +0100

    adds page layout base and general style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,21 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import { createRoutesFromChildren, Route, RouterProvider } from 'react-router'
 import { createBrowserRouter } from 'react-router-dom'
 import { MetricsSetup } from './pages/handle-metrics/MetricsSetup'
+import style from './App.module.scss'
 
 const queryClient = new QueryClient()
 
 const router = createBrowserRouter(
   createRoutesFromChildren(
     <Route errorElement={<div>Error page</div>}>
-      <Route path="/" element={<MetricsSetup />} />
+      <Route
+        path="/"
+        element={
+          <div className={style.pageBaseLayour}>
+            <MetricsSetup />
+          </div>
+        }
+      />
     </Route>
   )
 )
