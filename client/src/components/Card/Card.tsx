9af4d9219b766adc6198329f5bb82b95125ff7b4
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-02-12 14:48:54 +0100

    enhances Card component

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,19 +1,6 @@
 /** Total order card*/
+import { ReactNode } from 'react'
 import style from './Card.module.scss'
-export const Card = ({
-  code,
-  amounts,
-  date,
-}: {
-  code: string
-  amounts: number[] | null
-  date: string
-}) => {
-  return (
-    <div className={style.box}>
-      <h3>{code}</h3>
-      <p>Total amounts: {amounts?.reduce((acc, c) => c + acc, 0)}</p>
-      <div>Date: {new Intl.DateTimeFormat('it-IT').format(new Date(date))}</div>
-    </div>
-  )
+export const Card = ({ children }: { children: ReactNode }) => {
+  return <div className={style.box}>{children}</div>
 }
