6e47733f41ebe4b9df3d475eb8684c8ad087321d
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-02-14 12:19:14 +0100

    enhance Modal component

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,29 +1,26 @@
 import * as Dialog from '@radix-ui/react-dialog'
+
 import style from './Modal.module.scss'
 
-import React, { forwardRef } from 'react'
+import React from 'react'
 
 const Modal = ({ children, ...props }: Dialog.DialogProps) => {
   return <Dialog.Root {...props}>{children}</Dialog.Root>
 }
-export const Portal = ({ children, ...props }: Dialog.DialogPortalProps) => (
-  <Dialog.Portal {...props}>
-    <Dialog.Overlay className={style.overlay} />
-    {children}
-  </Dialog.Portal>
-)
-export const Content = forwardRef<
+
+export const Content = React.forwardRef<
   React.ForwardRefExoticComponent<
     Dialog.DialogContentProps & React.RefAttributes<HTMLDivElement>
   >,
   Dialog.DialogContentProps
->(({ children, ...props }, _) => {
-  return (
-    <Dialog.Content className={style.content} {...props}>
+>(({ children, ...props }, forwardedRef) => (
+  <Dialog.Portal>
+    <Dialog.Overlay className={style.overlay} />
+    <Dialog.Content className={style.content} {...props} ref={forwardedRef}>
       {children}
     </Dialog.Content>
-  )
-})
+  </Dialog.Portal>
+))
 
 export const Close = ({ children, ...props }: Dialog.DialogCloseProps) => (
   <Dialog.Close {...props}>{children}</Dialog.Close>
@@ -33,7 +30,6 @@ export const Trigger = ({ children, ...props }: Dialog.DialogTriggerProps) => {
   return <Dialog.Trigger {...props}>{children}</Dialog.Trigger>
 }
 
-Modal.Portal = Portal
 Modal.Content = Content
 Modal.Close = Close
 Modal.Trigger = Trigger
