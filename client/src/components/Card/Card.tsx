2380d15dc02a2a32bde55068903b19cf933b1585
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-02-14 12:18:44 +0100

    enhance Card component

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,6 +1,21 @@
 /** Total order card*/
 import { ReactNode } from 'react'
 import style from './Card.module.scss'
-export const Card = ({ children }: { children: ReactNode }) => {
+
+const Card = ({ children }: { children: ReactNode }) => {
   return <div className={style.box}>{children}</div>
 }
+
+export const Action = ({
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button className={style.button} {...props}>
+      {children}
+    </button>
+  )
+}
+
+Card.Action = Action
+export { Card }
