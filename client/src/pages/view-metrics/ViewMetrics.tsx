f8e6bd6d5e9108b9384ba2b72293c7ed66e7f492
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-02-16 01:06:50 +0100

    enhance ViewMetric page

--- client/src/pages/view-metrics/ViewMetrics.tsx
+++ client/src/pages/view-metrics/ViewMetrics.tsx
@@ -4,6 +4,25 @@ import { Loading } from '../../components/Loading'
 import { Table } from '../../components/Table/Table'
 import { Metric } from '../handle-metrics/MetricsSetup'
 import style from './ViewMetrics.module.scss'
+import { createColumnHelper } from '@tanstack/react-table'
+
+const columnHelper = createColumnHelper<Metric>()
+
+const columns = [
+  columnHelper.accessor('code', {
+    cell: (c) => c.getValue(),
+    header: () => <span>Marketing Campaing code</span>,
+  }),
+  columnHelper.accessor('amounts', {
+    cell: (c) => String(c.getValue()?.reduce((acc, a) => a + acc, 0) ?? 0),
+    header: () => <span>Total sales amounts</span>,
+  }),
+  columnHelper.accessor('date', {
+    cell: (c) =>
+      String(new Intl.DateTimeFormat('it-IT').format(new Date(c.getValue()))),
+    header: () => <span>Date</span>,
+  }),
+]
 
 export const MetricsView = () => {
   const { isLoading, error, data } = useQuery<Metric[], AxiosError>({
@@ -19,10 +38,16 @@ export const MetricsView = () => {
 
   return (
     <>
-      <h1 className={style.pageTitle}>Metrics pie</h1>
+      <h1 className={style.pageTitle}>Metrics details</h1>
       {isLoading && <Loading />}
-      <Table.Title>Metrics</Table.Title>
-      {data && <Table data={data} />}
+
+      {data && (
+        <>
+          <Table.Root>
+            <Table data={data} columns={columns} title="Metrics" />
+          </Table.Root>
+        </>
+      )}
     </>
   )
 }
